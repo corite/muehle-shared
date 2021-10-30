88bc768b48142846e499fc31fc27a33d650637e7
DESKTOP-UL7JO3U\bach0 <bach02@outlook.de>
2021-10-30 18:05:13 +0200

    Added Comments, changed resize Event, changed draw class

--- src/main/java/frontend/Draw.java
+++ src/main/java/frontend/Draw.java
@@ -8,22 +8,26 @@ public class Draw extends JLabel {
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+
         g.setColor(Color.BLACK);
-        g.drawLine(40,40,this.getWidth()-40,40);
-        g.drawLine(40,40,40,this.getWidth()-40);
-        g.drawLine(40,this.getWidth()-40,this.getWidth()-40,this.getWidth()-40);
-        g.drawLine(this.getWidth()-40,40,this.getWidth()-40,this.getWidth()-40);
-        g.drawLine(80,80,this.getWidth()-80,80);
-        g.drawLine(80,80,80,this.getWidth()-80);
-        g.drawLine(80,this.getWidth()-80,this.getWidth()-80,this.getWidth()-80);
-        g.drawLine(this.getWidth()-80,80,this.getWidth()-80,this.getWidth()-80);
-        g.drawLine(120,120,this.getWidth()-120,120);
-        g.drawLine(120,120,120,this.getWidth()-120);
-        g.drawLine(120,this.getWidth()-120,this.getWidth()-120,this.getWidth()-120);
-        g.drawLine(this.getWidth()-120,120,this.getWidth()-120,this.getWidth()-120);
-        g.drawLine(40,this.getWidth()/2,120,this.getWidth()/2);
-        g.drawLine(this.getWidth()/2,40,this.getWidth()/2,120);
-        g.drawLine(this.getWidth()-120,this.getWidth()/2,this.getWidth()-40,this.getWidth()/2);
-        g.drawLine(this.getWidth()/2,this.getWidth()-40,this.getWidth()/2,this.getWidth()-120);
+
+        //Äußerer Rahmen
+
+        g.drawRect(2*this.getWidth()/20,2*this.getHeight()/20, 16*this.getWidth()/20,16*this.getHeight()/20);
+
+        //Mittlerer Rahmen
+
+        g.drawRect(5*this.getWidth()/20,5*this.getHeight()/20,10*this.getWidth()/20,10*this.getHeight()/20);
+
+        //Innerer Rahmen
+
+        g.drawRect(8*this.getWidth()/20, 8*this.getHeight()/20, 4*this.getWidth()/20, 4*this.getHeight()/20);
+
+        //Verbindungslinien
+
+        g.drawLine(this.getWidth()/2,2*this.getHeight()/20,this.getWidth()/2,8*this.getHeight()/20);
+        g.drawLine(this.getWidth()/2,12*this.getHeight()/20,this.getWidth()/2,18*this.getHeight()/20);
+        g.drawLine(2*this.getWidth()/20,this.getHeight()/2,8*this.getWidth()/20,this.getHeight()/2);
+        g.drawLine(12*this.getWidth()/20,this.getHeight()/2,18*this.getWidth()/20,this.getHeight()/2);
     }
 }
--- src/main/java/frontend/GUI.java
+++ src/main/java/frontend/GUI.java
@@ -9,28 +9,46 @@ import java.awt.event.ComponentListener;
 public class GUI{
 
     public static void main(String[] args){
-
+        //Windowerstellung und Settings
         Draw draw = new Draw();
         JFrame frame = new JFrame("Muehle");
         frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        draw.setBounds(0,0,500,500);
+        draw.setVisible(true);
+        frame.add(draw);
+        //Die Components müssen zu Beginn erneut gezeichnet werden, da ansonsten nur die Hälfte angezeigt wird? Seid der
+        //Änderung des resize events
+        draw.paintComponent(draw.getGraphics());
+        //Abfangen des Window Resize Events
         frame.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
+                /* Lass ich mal noch so stehen, falls wir uns doch nochmal umentscheiden sollten
+                //Gleichsetzen der Werte von Höhe und Breite des Fensters um es quadratisch zu halten. Aufspaltung in
+                // 2 Fälle, da sont Höhe/Breite falsch Übertragen wird
                 if (frame.getHeight() < frame.getWidth()) {
                     frame.setSize(frame.getHeight(), frame.getHeight());
                 }
                 else{
                     frame.setSize(frame.getWidth(), frame.getWidth());
                 }
-                draw.setSize(frame.getWidth(), frame.getHeight());
+                //Das Gleiche für das JLabel
+                draw.setBounds(0,0,frame.getWidth(), frame.getHeight());
+                 */
+                //Wenn das JFrame größer in der Breite wird, wird das JLabel der Breite nach zentriert
+                if (frame.getWidth()>frame.getHeight()){
+                    draw.setBounds(frame.getWidth()/2-frame.getHeight()/2,0,frame.getHeight(),frame.getHeight());
+                }
+                //Andersherum, wenn das JFrame größer in der Höhe wird
+                else{
+                    draw.setBounds(0,frame.getHeight()/2-frame.getWidth(),frame.getWidth(),frame.getWidth());
+                }
             }
         });
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        draw.setBounds(0,0,640,480);
-        draw.setVisible(true);
-        frame.add(draw);
+        //Updaten des Mühle Bretts sobald das JLabel, bzw. JFrame resized wird
         draw.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
