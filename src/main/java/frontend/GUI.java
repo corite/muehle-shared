9015ad4e610d797c66a85bf0adcfee2fcf0dd430
Christian Ortlepp <christian.ortlepp@tutanota.com>
2021-11-07 17:34:35 +0100

    removed useless code

--- src/main/java/frontend/GUI.java
+++ src/main/java/frontend/GUI.java
@@ -1,5 +1,8 @@
 package frontend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
@@ -7,7 +10,6 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 public class GUI{
-
     public GUI(){
         Draw draw = new Draw();
         JFrame frame = new JFrame("Muehle");
@@ -20,33 +22,19 @@ public class GUI{
         frame.add(draw);
         //Die Components müssen zu Beginn erneut gezeichnet werden, da ansonsten nur die Hälfte angezeigt wird? Seid der
         //Änderung des resize events
-        draw.paintComponent(draw.getGraphics());
         //Abfangen des Window Resize Events
+
         frame.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
-            /* Lass ich mal noch so stehen, falls wir uns doch nochmal umentscheiden sollten
-            //Gleichsetzen der Werte von Höhe und Breite des Fensters um es quadratisch zu halten. Aufspaltung in
-            // 2 Fälle, da sont Höhe/Breite falsch Übertragen wird
-            if (frame.getHeight() < frame.getWidth()) {
-                frame.setSize(frame.getHeight(), frame.getHeight());
-            }
-            else{
-                frame.setSize(frame.getWidth(), frame.getWidth());
-            }
-            //Das Gleiche für das JLabel
-            draw.setBounds(0,0,frame.getWidth(), frame.getHeight());
-            */
                 //Wenn das JFrame größer in der Breite wird, wird das JLabel der Breite nach zentriert
                 if (frame.getWidth()>frame.getHeight()){
                     draw.setBounds(frame.getWidth()/2-frame.getHeight()/2,0,frame.getHeight(),frame.getHeight());
-                    draw.paintComponent(draw.getGraphics());
                 }
                 //Andersherum, wenn das JFrame größer in der Höhe wird
                 else{
                     draw.setBounds(0,frame.getHeight()/2-frame.getWidth()/2,frame.getWidth(),frame.getWidth());
-                    draw.paintComponent(draw.getGraphics());
                 }
             }
         });
